f4bb85126870f6c28b3c7814250d75ba3b72986f
danilo <danilodesenv@gmail.com>
2019-03-21 15:39:18 +0000

    Fixed in gradle multidex.

---
+++ app/src/main/java/desenv/danilo/carlisttypes/Teste.kt
@@ -0,0 +1,12 @@
+package desenv.danilo.carlisttypes
+
+import androidx.multidex.MultiDex
+import androidx.multidex.MultiDexApplication
+
+class CarApplication: MultiDexApplication() {
+
+    override fun onCreate() {
+        super.onCreate()
+        MultiDex.install(this)
+    }
+}
